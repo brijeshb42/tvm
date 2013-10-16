86d49e9885187e6a497482088f30ac9430b8311b
Brijesh Bittu <brijeshb42@gmail.com>
2013-10-16 15:34:30 -0700

    updated manifest with update url

--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-lessc less/main.less css/index.css && lessc less/dialog.less css/dialog.css && minify js/database.js js/database.min.js && minify js/index.js js/index.min.js && minify js/jquery.confirm.js js/jquery.confirm.min.js && minify js/jquery.console.js js/jquery.console.min.js
+lessc -x less/main.less css/index.css && lessc -x less/dialog.less css/dialog.css && minify js/database.js js/database.min.js && minify js/index.js js/index.min.js && minify js/jquery.confirm.js js/jquery.confirm.min.js && minify js/jquery.console.js js/jquery.console.min.js
